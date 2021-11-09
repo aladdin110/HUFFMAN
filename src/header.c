2fd6f93eee6e136c4e5b1ccb74a04080beb9ab52
alae <kouissi321@gmail.com>
2021-11-09 21:42:46 +0100

    the sort and getRedundancy functions

--- src/header.c
+++ src/header.c
@@ -1,5 +1,28 @@
 #include "header.h"
 
+charRedundancy* sort(charRedundancy* arr, int size) {
+    charRedundancy* array = (charRedundancy*)malloc(size * sizeof(charRedundancy));
+    int i, j;
+    for (i = 0; i < size; i++) {
+
+        int min_index = i;
+        for (j = i+1; j < size; j++) {
+            if (arr[j].r < arr[min_index].r) {
+                min_index = j;
+            }
+        }
+
+        if (min_index != i) {
+            charRedundancy tmp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = tmp;
+        }
+        array[i] = arr[i];
+    }
+    return array;
+}
+
+
 charRedundancy* getRedundancy(const char* str, int size) {
     char* string = (char*)malloc(size * sizeof(char));
     strcpy(string, str);
@@ -25,32 +48,12 @@ charRedundancy* getRedundancy(const char* str, int size) {
         index++;
     }
 
-    charRedundancy* array = (charRedundancy*)malloc(index* sizeof(charRedundancy));
-
-    for (i = 0; i < index; i++) {
-
-        int min_index = i;
-        for (j = i+1; j < index; j++) {
-            if (arr[j].r < arr[min_index].r) {
-                min_index = j;
-            }
-        }
-
-        if (min_index != i)
-        {
-            charRedundancy tmp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = tmp;
-        }
-
-        array[i] = arr[i];
-    }
+    charRedundancy* array = sort(arr, index);
 
     free(arr);
     free(string);
     return array;
 }
 
-charRedundancy* sort(charRedundancy* arr) {
 
-}
+
--- src/header.h
+++ src/header.h
@@ -5,12 +5,17 @@
 #include <string.h>
 
 // This is a structure that holds a character and its redundancy
-typedef struct {
+typedef struct _node{
     char c;
     int r;
+    struct _node* right;
+    struct _node* left;
 } charRedundancy;
 
 // This function takes a string and its size as parameters and return a charRedundancy sorted array in increasing order
 charRedundancy* getRedundancy(const char*, int);
+
+// This function takes an array of charRedundancy and its size and then return a sorted array
+charRedundancy* sort(charRedundancy*, int);
 
 #endif //HUFF_MAN_PROJECT_HEADER_H
